36449568248804da9eec4259965915978b43b169
MQ <mq@4mq.xyz>
2022-09-29 19:58:32 +0200

    get_geo util add okres to search

--- utils/get_geo.py
+++ utils/get_geo.py
@@ -25,6 +25,83 @@ KRAJE = {
         "CZ081": "Moravskoslezský kraj",
         }
 
+OKRESY = {
+        "CZ0100": "Hlavní město Praha",
+        "CZ0201": "Benešov",
+        "CZ0202": "Beroun",
+        "CZ0203": "Kladno",
+        "CZ0204": "Kolín",
+        "CZ0205": "Kutná Hora",
+        "CZ0206": "Mělník",
+        "CZ0207": "Mladá Boleslav",
+        "CZ0208": "Nymburk",
+        "CZ0209": "Praha-východ",
+        "CZ020A": "Praha-západ",
+        "CZ020B": "Příbram",
+        "CZ020C": "Rakovník",
+        "CZ0311": "České Budějovice",
+        "CZ0313": "Jindřichův Hradec",
+        "CZ0314": "Písek",
+        "CZ0315": "Prachatice",
+        "CZ0316": "Strakonice",
+        "CZ0317": "Tábor",
+        "CZ0321": "Domažlice",
+        "CZ0322": "Klatovy",
+        "CZ0323": "Plzeň-město",
+        "CZ0324": "Plzeň-jih",
+        "CZ0325": "Plzeň-sever",
+        "CZ0326": "Rokycany",
+        "CZ0327": "Tachov",
+        "CZ0411": "Cheb",
+        "CZ0412": "Karlovy Vary",
+        "CZ0413": "Sokolov",
+        "CZ0421": "Děčín",
+        "CZ0422": "Chomutov",
+        "CZ0423": "Litoměřice",
+        "CZ0424": "Louny",
+        "CZ0425": "Most",
+        "CZ0426": "Teplice",
+        "CZ0427": "Ústí nad Labem",
+        "CZ0511": "Česká Lípa",
+        "CZ0512": "Jablonec nad Nisou",
+        "CZ0513": "Liberec",
+        "CZ0514": "Semily",
+        "CZ0521": "Hradec Králové",
+        "CZ0522": "Jičín",
+        "CZ0523": "Náchod",
+        "CZ0524": "Rychnov nad Kněžnou",
+        "CZ0525": "Trutnov",
+        "CZ0531": "Chrudim",
+        "CZ0532": "Pardubice",
+        "CZ0533": "Svitavy",
+        "CZ0534": "Ústí nad Orlicí",
+        "CZ0631": "Havlíčkův Brod",
+        "CZ0632": "Jihlava",
+        "CZ0633": "Pelhřimov",
+        "CZ0634": "Třebíč",
+        "CZ0635": "Žďár nad Sázavou",
+        "CZ0641": "Blansko",
+        "CZ0642": "Brno-město",
+        "CZ0644": "Břeclav",
+        "CZ0645": "Hodonín",
+        "CZ0646": "Vyškov",
+        "CZ0647": "Znojmo",
+        "CZ0711": "Jeseník",
+        "CZ0712": "Olomouc",
+        "CZ0713": "Prostějov",
+        "CZ0714": "Přerov",
+        "CZ0715": "Šumperk",
+        "CZ0721": "Kroměříž",
+        "CZ0722": "Uherské Hradiště",
+        "CZ0723": "Vsetín",
+        "CZ0724": "Zlín",
+        "CZ0801": "Bruntál",
+        "CZ0802": "Frýdek-Místek",
+        "CZ0803": "Karviná",
+        "CZ0804": "Nový Jičín",
+        "CZ0806": "Ostrava-město",
+        }
+
 
 def read_json(filepath):
     if os.path.exists(filepath):
@@ -90,8 +167,10 @@ def main(args):
     try:
         for subjekt in subjekty:
             info = subjekt_info(subjekt)
-            okres = info["reditelstvi"]["okres"]
-            kraj = KRAJE[okres[:5]]
+            lau = info["reditelstvi"]["okres"]
+            nuts = lau[:5]
+            kraj = KRAJE.get(nuts)
+            okres = OKRESY.get(lau)
 
             for zarizeni in info.get("zarizeni", []):
                 for misto in zarizeni.get("mista", []):
@@ -109,7 +188,10 @@ def main(args):
                     adr3 = misto["adr3"]
 
                     adr = ",".join(filter(None, [adr1, adr2, adr3]))
-                    adr += ",%s" % kraj
+                    if okres:
+                        adr += ",%s" % okres
+                    if kraj:
+                        adr += ",%s" % kraj
 
                     time.sleep(1)
                     geo_loc = geo_lookup(adr)
